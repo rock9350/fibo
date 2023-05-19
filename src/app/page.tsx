dc9db7f5e95d6b62c66e60bf43d92f6f62c9adb9
rock9350 <ROCK9350136274@gmail.com>
2023-05-19 17:46:45 +0530

    12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,142 +3,147 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import data from "./data";
 
-export default function Home() {
-  return (
-    <main>
+const Brand = ({ shopName  }) => (
+  <div className={styles.brand}>
+    <div className={styles["brand-log"]} id="Logo">
+      <Image src="/img/Ellipse 157.png" width={50} height={50} alt="aa" />
+    </div>
+    <div className={styles["brand-name-box"]}>
+      <h3 className={styles["brand-name"]}>{shopName}</h3>
+      <h5>Food Influencer</h5>
+    </div>
+  </div>
+);
+
+const FoodItem = ({ food }) => (
+  <div className={styles.food}>
+    <div>
       <Image
-        src="/img/Rectangle 5629.jpg"
-        width={500}
-        height={900}
-        style={{ position: "fixed", zIndex: "-1" }}
+        src="/img/Ellipse 28.png"
+        width={56}
+        height={56}
+        alt="aa"
+        className={styles.foodLogo}
+      />
+    </div>
+    <div className={styles.foodName}>
+      <p>{food.foodName}</p>
+      <p className={styles.foodTime}>{food.foodTime}</p>
+    </div>
+    <div className={styles.foodDetailsBox}>
+      <div className={styles.foodKcal}>
+        <p>{food.foodKcal}</p>
+      </div>
+      <Image
+        src="/img/plus.png"
+        width={12}
+        height={12}
+        alt="aaa"
+      />
+    </div>
+  </div>
+);
+
+const Coupon = () => (
+  <div className={styles.couponBox}>
+    <h2>Meals Coupon</h2>
+    <div id="offe" className={styles.offers}>
+      <div className={styles.days}>
+        <h6>27th Jan - 18th Feb</h6>
+        <div>
+          <p>UPTO </p>
+          <p>25% OFF </p>
+        </div>
+      </div>
+
+      <div className={styles.coupon}>
+        <p className={styles.couponOffer}>Chinese</p>
+        <h3>₹299/-</h3>
+        <p className={styles.couponOffer}>
+          22% + 30% off - Expires in 1h
+        </p>
+      </div>
+      <div className={styles.coupon}>
+        <p className={styles.couponOffer}>Chinese</p>
+        <h3>₹299/-</h3>
+        <p className={styles.couponOffer}>
+          22% + 30% off - Expires in 1h
+        </p>
+      </div>
+      <div className={styles.coupon}>
+        <p className={styles.couponOffer}>Chinese</p>
+        <h3>₹299/-</h3>
+        <p className={styles.couponOffer}>
+          22% + 30% off - Expires in 1h
+        </p>
+      </div>
+      <div className={styles.coupon}>
+        <p className={styles.couponOffer}>Chinese</p>
+        <h3>₹299/-</h3>
+        <p className={styles.couponOffer}>
+          22% + 30% off - Expires in 1h
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+const Home = () => (
+  <main>
+    <Image
+      src="/img/Rectangle 5629.jpg"
+      width={500}
+      height={900}
+      style={{ position: "fixed", zIndex: "-1" }}
+      alt="aa"
+    />
+
+    <div>
+      <Image
+        src="/img/Vector.png"
+        className={styles["share"]}
+        width={15}
+        height={16}
         alt="aa"
       />
 
-      <div>
-        <Image
-          src="/img/Vector.png"
-          className={styles["share"]}
-          width={15}
-          height={16}
-          alt="aa"
-        />
+      {data.map((item) => (
+        <>
+          <Brand shopName={item.shopName} />
 
-        {data.map((item) => (
-          <>
-            <div className={styles.brand}>
-              <div className={styles["brand-log"]} id="Logo">
-                <Image
-                  src="/img/Ellipse 157.png"
-                  width={50}
-                  height={50}
-                  alt="aa"
-                />
-              </div>
-              <div className={styles["brand-name-box"]}>
-                <h3 className={styles["brand-name"]}>{item.shopName}</h3>
-                <h5>Food Influencer</h5>
-              </div>
+          <div className={styles["item-box"]}>
+            <div className={styles.stroke}>
+              <Image
+                src="/img/Stroke 1.png"
+                width={13}
+                height={7}
+                alt="aaa"
+              />
             </div>
-
-            <div className={styles["item-box"]}>
-              <div className={styles.stroke}>
-                <Image
-                  src="/img/Stroke 1.png"
-                  width={13}
-                  height={7}
-                  alt="aaa"
-                />
-              </div>
-              <div className={styles["item-details"]}>
-                <h2>Food Centre</h2>
-              </div>
-              {item.itemsType.map((items) => (
-                <>
-                  <div className="items-type-box">
-                    <div className={styles["food-type-box"]}>
-                      <h2>{items.type}</h2>
-                      <button>{items.countryType}</button>
-                    </div>
-                    {items.food.map((ite) => (
-                      <div key={ite.foodName} className={styles.food}>
-                        <div>
-                          <Image
-                            src="/img/Ellipse 28.png"
-                            width={56}
-                            height={56}
-                            alt="aa"
-                            className={styles.foodLogo}
-                          />
-                        </div>
-                        <div className={styles.foodName}>
-                          <p>{ite.foodName}</p>
-                          <p className={styles.foodTime}>{ite.foodTime}</p>
-                        </div>
-                        <div className={styles.foodDetailsBox}>
-                          <div className={styles.foodKcal}>
-                            <p>{ite.foodKcal}</p>
-                          </div>
-                          <Image
-                            src="/img/plus.png"
-                            width={12}
-                            height={12}
-                            alt="aaa"
-                          />
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              ))}
-
-              <div className={styles.couponBox}>
-                <h2>Meals Coupon</h2>
-                <div id="offe" className={styles.offers}>
-                  <div className={styles.days}>
-                    <h6>27th Jan - 18th Feb</h6>
-                    <div>
-                      <p>UPTO </p>
-                      <p>25% OFF </p>
-                    </div>
-                  </div>
-
-                  <div className={styles.coupon}>
-                    <p className={styles.couponOffer}>Chinese</p>
-                    <h3>₹299/-</h3>
-                    <p className={styles.couponOffer}>
-                      22% + 30% off - Expires in 1h
-                    </p>
-                  </div>
-                  <div className={styles.coupon}>
-                    <p className={styles.couponOffer}>Chinese</p>
-                    <h3>₹299/-</h3>
-                    <p className={styles.couponOffer}>
-                      22% + 30% off - Expires in 1h
-                    </p>
-                  </div>
-                  <div className={styles.coupon}>
-                    <p className={styles.couponOffer}>Chinese</p>
-                    <h3>₹299/-</h3>
-                    <p className={styles.couponOffer}>
-                      22% + 30% off - Expires in 1h
-                    </p>
-                  </div>
-                  <div className={styles.coupon}>
-                    <p className={styles.couponOffer}>Chinese</p>
-                    <h3>₹299/-</h3>
-                    <p className={styles.couponOffer}>
-                      22% + 30% off - Expires in 1h
-                    </p>
-                  </div>
+            <div className={styles["item-details"]}>
+              <h2>Food Centre</h2>
+            </div>
+            {item.itemsType.map((items) => (
+              <div className="items-type-box">
+                <div className={styles["food-type-box"]}>
+                  <h2>{items.type}</h2>
+                  <button>{items.countryType}</button>
                 </div>
+                {items.food.map((ite) => (
+                  <FoodItem key={ite.foodName} food={ite} />
+                ))}
               </div>
-              <div>
-                <button className={styles["order"]}>Order Now</button>
-              </div>
+            ))}
+
+            <Coupon />
+            <div>
+              <button className={styles["order"]}>Order Now</button>
             </div>
-          </>
-        ))}
-      </div>
-    </main>
-  );
-}
+          </div>
+        </>
+      ))}
+    </div>
+  </main>
+);
+
+export default Home;
